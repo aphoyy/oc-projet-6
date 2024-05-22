22e3e8b4ce7948033e3314ccb96ab61d841a84b3
aphoyy <christophe.scherrer4@gmail.com>
2024-05-22 12:26:07 +0200

    Add filter buttons

--- FrontEnd/JS/main.js
+++ FrontEnd/JS/main.js
@@ -1,7 +1,9 @@
 let projectsList = [];
 let categoriesList = [];
+let previousFilter = 0;
 
 window.onload = getProjects();
+window.onload = getCategories();
 
 async function getProjects() {
 	const response = await fetch("http://localhost:5678/api/works");
@@ -29,4 +31,37 @@ function displayProjects() {
 async function getCategories() {
 	const response = await fetch("http://localhost:5678/api/categories");
 	categoriesList  = await response.json();
+    displayCategories();
+}
+
+function displayCategories() {
+    const categoriesDiv = document.getElementById("categories");
+    categoriesDiv.insertAdjacentHTML(
+        "beforeend",
+        `<button id="filter-0" onclick="filterClick(0)" class="filter-selected">Tous</button>`
+    );
+    for (i = 0; i < categoriesList.length; i++) {
+        const category = 
+            `<button id="filter-` +
+            (i + 1) +
+            `" onclick="filterClick(` +
+            (i + 1) +
+            `)">`+
+            categoriesList[i]["name"] +
+            "</button>";
+        categoriesDiv.insertAdjacentHTML("beforeend", category);
+    }
+}
+
+function filterClick(value) {
+    console.log(value)
+    // Remove class from previous filter
+    document
+        .getElementById("filter-" + previousFilter).classList
+        .remove("filter-selected");
+    // Add class to new filter
+    document
+        .getElementById("filter-" + value).classList
+        .add("filter-selected");
+    previousFilter = value;
 }
